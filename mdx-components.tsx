d9d0297e10f4db85d07bcf2d9178a86ddb1aefd5
Julien Thibeaut <julien.thibeaut@gmail.com>
2025-03-27 14:38:21 +0100

    feat: add jsx-preview (#27)
    * feat: add jsx-preview
    
    * feat: add jsx-preview doc and example
    
    * feat: build registry
    
    * fix: build

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -9,15 +9,36 @@ import { extractCodeFromFilePath } from "./lib/code"
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    code: ({ children, ...props }) => (
+    code: ({ children, ...props }: React.HTMLAttributes<HTMLElement>) => (
       <code {...props} className="not-prose bg-secondary p-1 font-mono">
         {children}
       </code>
     ),
-    a: ({ children, ...props }) => <Link {...props}>{children}</Link>,
-    Link: ({ children, ...props }) => <Link {...props}>{children}</Link>,
-    CodeBlock: async ({ language, code, filePath, ...props }) => {
-      const fileContent = filePath ? extractCodeFromFilePath(filePath) : code
+    // @ts-ignore
+    a: (props: React.ComponentProps<typeof Link>) => (
+      <Link {...props} href={props.href || ""}>
+        {props.children}
+      </Link>
+    ),
+    Link: ({ children, ...props }: React.ComponentProps<"a">) => (
+      <Link {...props} href={props.href as string}>
+        {children}
+      </Link>
+    ),
+    // @ts-ignore
+    CodeBlock: async ({
+      language,
+      code,
+      filePath,
+      ...props
+    }: {
+      language: string
+      code: string
+      filePath?: string
+    } & React.HTMLAttributes<HTMLDivElement>) => {
+      const fileContent = filePath
+        ? extractCodeFromFilePath(filePath)
+        : code || ""
       const html = await codeToHtml({ code: fileContent, lang: language })
 
       return (
@@ -62,12 +83,18 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     }: React.ComponentProps<typeof TabsContent>) => (
       <TabsContent className={cn(className)} {...props} />
     ),
-    table: ({ className, ...props }: React.ComponentProps<"table">) => (
+    table: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableElement>) => (
       <div className="not-prose relative w-full table-auto overflow-auto rounded-lg border border-zinc-200 text-sm dark:border-zinc-800">
         <table className={cn("w-full", className)} {...props} />
       </div>
     ),
-    thead: ({ className, ...props }: React.ComponentProps<"thead">) => (
+    thead: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableSectionElement>) => (
       <thead
         className={cn(
           "bg-zinc-100 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100",
@@ -76,7 +103,10 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         {...props}
       />
     ),
-    tbody: ({ className, ...props }: React.ComponentProps<"tbody">) => (
+    tbody: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableSectionElement>) => (
       <tbody
         className={cn(
           "divide-y divide-zinc-200 dark:divide-y dark:divide-zinc-600",
@@ -85,16 +115,25 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         {...props}
       />
     ),
-    tr: ({ className, ...props }: React.ComponentProps<"tr">) => (
+    tr: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableRowElement>) => (
       <tr className={cn("h-10", className)} {...props} />
     ),
-    th: ({ className, ...props }: React.ComponentProps<"th">) => (
+    th: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableHeaderCellElement>) => (
       <th
         className={cn("px-4 pb-0 text-left align-middle font-[450]", className)}
         {...props}
       />
     ),
-    td: ({ className, ...props }: React.ComponentProps<"td">) => (
+    td: ({
+      className,
+      ...props
+    }: React.HTMLAttributes<HTMLTableDataCellElement>) => (
       <td
         className={cn("px-4 py-2 text-left align-middle", className)}
         {...props}
