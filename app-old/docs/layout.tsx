ddf75554c581f83bf8a6ff487bd83cec82e7da63
Pushpak Chhajed <pushpak1300@gmail.com>
2025-07-19 18:46:53 +0530

    chore: remove old files and update .gitignore
    - Deleted old application files from app-old directory, including components, pages, and assets.
    - Updated .gitignore to include new directories for old files.

--- app-old/docs/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { LayoutProse } from "@/components/app/layout-prose"
-
-export default function LayoutDocs({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <LayoutProse>{children}</LayoutProse>
-}
